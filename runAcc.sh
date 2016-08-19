5c84cae90511379fe2a5106ef86b3eeba39c9255
Lauren Zhang <lwz2103@barnard.edu>
2016-08-19 17:46:08 -0400

    debugging - try making floats smaller

--- runAcc.sh
+++ runAcc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #$ -cwd -S /bin/bash -j y
 ##$ -pe smp 3
-#$ -l mem=30G,time=:10:
+#$ -l mem=25G,time=:10:
 #$ -l gpu=1
 #$ -M lwz2103@c2b2.columbia.edu -m bes
 
 
 # the program to run
-./accuracy.o 60 100000 400
+./accuracy.o 60 1000000 400
