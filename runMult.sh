c2113ca26676bc78e3c1f512dd19786a46a27e80
Lauren Zhang <lwz2103@barnard.edu>
2016-08-25 15:02:18 -0400

    testing cuBLAS code

---
+++ runMult.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -cwd -S /bin/bash -j y
+##$ -pe smp 3
+#$ -l mem=25G,time=:10:
+#$ -l gpu=1
+#$ -M lwz2103@c2b2.columbia.edu -m bes
+
+
+# the program to run
+# ./accuracy.o 50 1000000 400
+
+# check for memory leaks
+cuda-memcheck ./accuracy.o 50 1000000 400
