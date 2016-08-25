0e6f31d0139023746bd98dfe4b6b7eaf3287d6dc
Lauren Zhang <lwz2103@barnard.edu>
2016-08-25 15:05:41 -0400

    forgot to define Matrix struct

--- mult.cu
+++ mult.cu
@@ -6,6 +6,15 @@
 #include <cublas_v2.h>
 using namespace std;
 
+// Matrices are stored in row-major order:
+// M(row, col) = *(M.elements + row * M.stride + col)
+typedef struct {
+    int width;
+    int height;
+    int stride; 
+    float* elements;
+} Matrix;
+
 void MatMul(const Matrix A, const Matrix B, Matrix C)
 {
 	cudaError_t err;
